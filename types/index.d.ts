8fa44f9a271b961d0d4dd2f044a26cdb14677ae2
Hai Pham <swiften.svc@gmail.com>
2021-12-18 18:52:45 +0800

    Update type file for more accurate typecheck

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,15 @@
-export default FirebaseUIAuth;
-declare function FirebaseUIAuth({ auth, config, lang, version, rtl, firebase }: {
-    auth: any;
-    config: any;
-    lang: any;
-    version: any;
-    rtl: any;
-    firebase: any;
-}): any;
+import * as firebase from "firebase/app";
+import { Auth } from "firebase/auth";
+import firebaseui from "firebaseui";
+
+declare module "react-firebaseui-localized" {
+  interface Props {
+    readonly auth: Auth;
+    readonly config: firebaseui.auth.Config;
+    readonly firebase: typeof firebase;
+    readonly lang: "en";
+    readonly version: "6.0.0";
+  }
+
+  export default function (props: Props): JSX.Element;
+}
